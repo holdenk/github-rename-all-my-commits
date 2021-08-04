6acec017335fbf5fd327c2051c319bb13153f70e
Holden Karau <holden@pigscanfly.ca>
2021-08-03 17:38:56 -0700

    Initial shell script to rewrite your commits from your old github account to your new one. I haven't tested this much so be careful

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # github-rename-all-my-commits
-Uses git filter-repo to rename all of your commits in all of your repos, intended for removing deadnames, will be funky with any forks you want to merge though.
+
+Uses git filter-repo depending on system availability to rename all of your commits in all of your repos, intended for removing deadnames, will be funky with any forks you want to merge though.
+
+
+This only runs on public repos.
+
+# Warnings
+
+This is not tested, I wrote this in between doing other crap for a friend.
+
+# Install
+
+git filter-repo requires python to be installed, fetching all of the user repos depends on "jq"
+
+See the https://github.com/newren/git-filter-repo/blob/main/INSTALL.md for installing `filter-repo`.
+
+See https://stedolan.github.io/jq/ for how to instal `jq`.
+
+Also requires WSL to execute bash.
+
+# Running
+
+All of the "logic" is in `runme.sh`. Note no checks are performed to verify that new and from github usernames are different, but I strongly encourage that because this force pushes a lot.
+
+You may specify multiple old e-mails.
+
+`runme.sh --from-github-username [OLDGITHUBUSERNAME] --to-github-username [NEWGITHUBUSERNAME] --new-name "[NEWNAME]" --new-email [NEWEMAIL] --old-email [OLDEMAIL]`.
+
+
+For password you must either specify `--github-netrc` pointing to a netrc file used for github.com login or `--github-token` specifying your github token (see https://github.com/settings/tokens to create one). Must have repo permissions.
---
+++ runme.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+set -x
+tmpdir=$(mktemp -d)
+virtualenv "${tmpdir}/venv"
+source "${tmpdir}/venv/bin/activate"
+pip install -r requirements.txt
+
+OLD_EMAILS=()
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]; do
+  key="$1"
+
+  case $key in
+    --from-github-username)
+      FROM_GITHUB_USERNAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --to-github-username)
+      TO_GITHUB_USERNAME="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --to-github-token)
+      TO_GITHUB_PASSWORD="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --to-github-netrc)
+      TO_GITHUB_NETRC="$2"
+      shift # past argument
+      shift # past value
+      ;;
+    --new-name)
+      NEW_NAME="$2"
+      shift;
+      shift;
+      ;;
+   --new-email)
+     NEW_EMAIL="$2"
+     shift;
+     shift;
+     ;;
+   --old-email)
+     OLD_EMAILS+=("$2")
+     shift;
+     shift;
+     ;;
+  esac
+done
+
+echo "Running with FROM_GITHUB_USERNAME=$FROM_GITHUB_USERNAME TO_GITHUB_USERNAME=$TO_GITHUB_USERNAME NEW_NAME=$NEW_NAME NEW_EMAIL=$NEW_EMAIL OLD_EMAIL=$OLD_EMAILS"
+
+read -p "Press enter to continue or ctrl-c to quit"
+
+pushd "${tmpdir}"
+
+if [ -z "${TO_GITHUB_NETRC}" ]; then
+  if [ -z "${TO_GITHUB_PASSWORD}"] ; then
+    echo "You must specify one of --to-github-netrc or --to-github-token"
+    exit 1
+  fi
+  TO_GITHUB_NETRC="${tmpdir}/netrc"
+  echo "machine api.github.com
+login ${TO_GITHUB_USERNAME}
+password ${TO_GITHUB_PASSWORD}" > ${TO_GITHUB_NETRC}
+fi
+
+# Create the rewrite file
+
+for email in "${OLD_EMAILS[@]}"
+do
+  echo "${NEW_NAME} <${NEW_EMAIL}> <${email}>" >> ${tmpdir}/rewritemap
+done
+
+echo "Using rewrite map $(cat ${tmpdir}/rewritemap)"
+echo "See README.md for info on how to configure"
+read -p "Press enter to continue or ctrl-c to quit"
+
+# Rewrite the repos
+
+PAGE=1
+
+
+while true; do
+  #  RESULTS=$(curl -s "https://api.github.com/users/${FROM_GITHUB_USERNAME}/repos?per_page=200&page=${PAGE}" | jq -r ".[].name")
+  RESULTS=$(curl -s "https://api.github.com/orgs/apache/repos?per_page=200&page=${PAGE}" | jq -r ".[].name")
+  if [ -z "$RESULTS" ]; then
+    break
+  fi
+  while IFS= read -r repo_name; do
+    git clone "git@github.com:${FROM_GITHUB_USERNAME}/${repo_name}.git"
+    cd "${repo_name}"
+    git filter-repo --mailmap "${tmpdir}/rewritemap"
+    git remote add newuser "git@github.com:${TO_GITHUB_USERNAME}/${repo_name}.git"
+    curl --netrc-file "${TO_GITHUB_NETRC}" https://api.github.com/user/repos -d "{\"name\":\"${repo_name}\"}"
+    git push --all newuser
+  done <<< "$RESULTS"
+  PAGE=$((PAGE + 1))
+done
